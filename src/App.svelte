38c77615ebbb0d3c3bcf90c7084d655f5b3dc29e
dead end <dead.end@inbox.ru>
2021-09-19 21:40:21 +0200

    initial version

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,25 @@
 <script>
-	export let name;
+  import { onMount } from 'svelte';
+  import { RegistryStore } from './stores/registryStore';
+
+  import Header from './components/Header.svelte';
+  import Footer from './components/Footer.svelte';
+  import Registry from './components/Registration.svelte';
+
+  onMount(RegistryStore.load);
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div class="container">
+  <Header />
+
+  <main>
+    {#each $RegistryStore as registration}
+      <Registry {registration} />
+    {/each}
+  </main>
+
+  <Footer />
+</div>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
