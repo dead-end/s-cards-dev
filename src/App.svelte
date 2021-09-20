dffcd99611d766bf605d641d3fb70d4c93188fd2
dead end <dead.end@inbox.ru>
2021-09-20 21:31:33 +0200

    added topic list

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,14 @@
 <script>
-  import { onMount } from 'svelte';
-  import { TopicStore } from './stores/topicStore';
-
   import Header from './components/Header.svelte';
   import Footer from './components/Footer.svelte';
-  import Topic from './components/Topic.svelte';
-
-  onMount(TopicStore.load);
+  import TopicList from './components/TopicList.svelte';
 </script>
 
 <div class="container">
   <Header />
 
   <div id="main" class="block">
-    <div class="grid grid-4" id="pool-list-body">
-      {#each $TopicStore as topic}
-        <Topic {topic} />
-      {/each}
-    </div>
+    <TopicList />
   </div>
 
   <Footer />
