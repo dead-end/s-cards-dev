496897c84247e708198fc7639143748ab8cf0104
dead end <dead.end@inbox.ru>
2021-09-20 21:31:18 +0200

    added topic list

---
+++ src/components/TopicList.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { onMount } from 'svelte';
+  import { TopicStore } from './stores/topicStore';
+  import Topic from './components/Topic.svelte';
+
+  onMount(TopicStore.load);
+</script>
+
+<div class="grid grid-4" id="pool-list-body">
+  {#each $TopicStore as topic}
+    <Topic {topic} />
+  {/each}
+</div>
