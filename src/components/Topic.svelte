46f28ba7994bc8de14032d16caf86e1010d59399
dead end <dead.end@inbox.ru>
2021-09-20 21:26:36 +0200

    update

--- src/components/Topic.svelte
+++ src/components/Topic.svelte
@@ -2,8 +2,22 @@
   export let topic;
 </script>
 
-<h4>{topic.title}</h4>
-<p>{topic.file}</p>
-{#if topic.desc}
-  <p>{topic.desc}</p>
-{/if}
+<div class="card card-shadow content">
+  <h4>{topic.title}</h4>
+  <table>
+    <tr>
+      <td>Modified</td>
+      <td />
+    </tr>
+    <tr>
+      <td>Status</td>
+      <td />
+    </tr>
+  </table>
+  {#if topic.desc}
+    <p>{topic.desc}</p>
+  {/if}
+  <div class="buttons">
+    <button class="button">Show</button>
+  </div>
+</div>
