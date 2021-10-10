76be0d24235d519419ea1fb9acca71cbbbb43d9d
dead end <dead.end@inbox.ru>
2021-10-10 21:03:33 +0200

    removed db

--- src/js/persist.js
+++ src/js/persist.js
@@ -2,8 +2,7 @@ import { fetchLastModified, fetchJson } from './fetch';
 import { storeDeleteIndex, storeAddAll } from './store';
 import { dbtGetLastModified, dbtSetLastModified, dbtSync } from './dbTopics';
 import { dbcGetLastModified, dbcSetLastModified } from './dbConfig.js';
-
-let db;
+import { db, dbInit } from './db';
 
 /**
  *
@@ -12,7 +11,7 @@ let db;
 export const loadQuestions = async (file) => {
   console.log(file);
 
-  const storeLm = dbtGetLastModified(db, file);
+  const storeLm = dbtGetLastModified(file);
   const jsonLm = fetchLastModified(file);
 
   const [lmStore, lmJson] = await Promise.all([storeLm, jsonLm]);
@@ -45,10 +44,10 @@ export const loadQuestions = async (file) => {
 
 // -----------------------------
 
-// Replace by:
+export const initApp = async () => {
+  await dbInit();
 
-const initApp = async () => {
-  const storeLmPromise = dbcGetLastModified(db);
+  const storeLmPromise = dbcGetLastModified();
 
   const headLmPromise = fetchLastModified('data/topics.json');
 
@@ -59,102 +58,8 @@ const initApp = async () => {
 
   if (!storeLm || storeLm < headLm) {
     fetchJson('data/topics.json').then((json) => {
-      dbtSync(db, json);
-      dbcSetLastModified(db, headLm);
+      dbtSync(json);
+      dbcSetLastModified(headLm);
     });
   }
-};
-
-// ------------------------------------
-
-/**
- * Simple error callback function.
- *
- * @param {*} event
- */
-const onError = (e) => {
-  console.error(e.target.error);
-};
-
-/**
- * The function create the object stores and indices.
- *
- * @param {IDBVersionChangeEvent} e
- */
-const onUpgradeNeeded = (e) => {
-  if (e.oldVersion < 1) {
-    db = e.target.result;
-
-    //
-    // Create topics store
-    //
-    const storeTopics = db.createObjectStore('topics', {
-      keyPath: 'file',
-    });
-
-    //
-    // Create questions store
-    //
-    const storeQuest = db.createObjectStore('questions', {
-      keyPath: 'id',
-    });
-    storeQuest.createIndex('file', 'file', { unique: false });
-
-    //
-    // Create progress store
-    //
-    const storeProgress = db.createObjectStore('progress', {
-      keyPath: 'id',
-    });
-    storeProgress.createIndex('file', 'file', { unique: false });
-
-    //
-    // Create config store
-    //
-    const storeConfig = db.createObjectStore('config', {
-      keyPath: 'key',
-    });
-
-    storeConfig.transaction.oncomplete = () => {
-      console.log('Upgrade completed!');
-    };
-  }
-};
-
-/**
- * The function iniitalizes the indexed db.
- */
-export const initDB = () => {
-  //
-  // Open db request for version 1.
-  //
-  const request = indexedDB.open('s-card', 1);
-
-  //
-  // Callback function for creating or upgrading the db.
-  //
-  request.onupgradeneeded = onUpgradeNeeded;
-
-  //
-  // Error handling callback function for the opening request.
-  //
-  request.onerror = onError;
-
-  request.onsuccess = (e) => {
-    //
-    //
-    //
-    db = e.target.result;
-
-    //
-    // Centeralized error handling callback function.
-    //
-    db.onerror = onError;
-    console.log('db init success!');
-
-    //
-    // Initialize the application.
-    //
-    initApp();
-  };
 };
