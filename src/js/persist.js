7da98b5c6f1f6dc5a18ed19e0d3bf16e4af60923
dead end <dead.end@inbox.ru>
2021-10-12 22:13:54 +0200

    update

--- src/js/persist.js
+++ src/js/persist.js
@@ -5,40 +5,57 @@ import { dbcGetLastModified, dbcSetLastModified } from './dbConfig.js';
 import { db, dbInit } from './db';
 
 /**
+ * The function is called with the file name of a topic. It checks if an update
+ * of the stored questions of a topic is necessary and if so, if does the
+ * update.
  *
- * @param {*} file
+ * @param {String} file
  */
 export const loadQuestions = async (file) => {
   console.log(file);
 
+  //
+  // Compare the last modified date of the stored topic file, with the last
+  // modified date of the file on the server, to decide if an update is
+  // necessary.
+  //
   const storeLm = dbtGetLastModified(file);
   const jsonLm = fetchLastModified(file);
 
   const [lmStore, lmJson] = await Promise.all([storeLm, jsonLm]);
-
   console.log('lmStore', lmStore, 'lmJson', lmJson);
+
   if (lmStore && lmStore >= lmJson) {
     return;
   }
-// TODO
-  const jsonPromise = await fetchJson(file);
 
+  //
+  // At this point we know that we have to update the questions for the topic.
+  //
   const tx = db.transaction(['topics', 'questions', 'progress'], 'readwrite');
 
-  const promDelQuest = storeDeleteIndex(tx, 'questions', 'file', file);
-  const promDelProgress = storeDeleteIndex(tx, 'progress', 'file', file);
+  const delQuestsPromise = storeDeleteIndex(tx, 'questions', 'file', file);
+  const delProgressPromise = storeDeleteIndex(tx, 'progress', 'file', file);
+  const jsonPromise = fetchJson(file);
 
+  //
+  // All three things can be done in parallel. Only the json promise returns
+  // something.
+  //
   const [json] = await Promise.all([
     jsonPromise,
-    promDelQuest,
-    promDelProgress,
+    delQuestsPromise,
+    delProgressPromise,
   ]);
 
+  json.forEach((j) => (j.file = file));
   console.log(json);
 
-  json.forEach((j) => (j.file = file));
-
   storeAddAll(tx, 'questions', json);
+
+  //
+  // The last step is to update the last modified value for the topic file.
+  //
   dbtSetLastModified(tx, file, lmJson);
 };
 
