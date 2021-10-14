4019a31df3f1c566c81e2b47134d02fb5dfed19f
dead end <dead.end@inbox.ru>
2021-10-14 22:23:11 +0200

    update

--- src/components/TopicShow.svelte
+++ src/components/TopicShow.svelte
@@ -2,13 +2,30 @@
   import { viewStore } from '../stores/viewStore';
 
   import { loadQuestions } from '../js/persist';
-  import { fmtDate } from '../js/utils';
+  import { dbqSetCurrent, dbqGetStats } from '../js/dbQuest';
+  import { fmtDate, arrPercentage, arrAll } from '../js/utils';
 
   import { onMount } from 'svelte';
 
   export let topic;
+  // TODO: comments
 
-  onMount(() => loadQuestions(topic.file));
+  let status = '';
+  let startDisabled = true;
+  let size = 0;
+
+  const updateStatus = () => {
+    dbqGetStats().then((arr) => {
+      status = arrPercentage(arr, 3);
+      startDisabled = arrAll(arr, 3);
+      size = arr.length;
+    });
+  };
+
+  onMount(() => {
+    loadQuestions(topic.file);
+    updateStatus();
+  });
 
   const onBack = () => {
     viewStore.setView('TopicList');
@@ -16,6 +33,21 @@
 
   const onListing = (topic) => {
     viewStore.setView('QuestList', { topic: topic });
+  };
+
+  const onSelect = (e) => {
+    console.log('index', e.target.selectedIndex);
+    //
+    // Set the number of correct answers.
+    //
+    dbqSetCurrent(topic.file, e.target.selectedIndex - 1);
+
+    //
+    // Set the index to 0 to restore the orignal state.
+    //
+    e.target.selectedIndex = 0;
+
+    updateStatus();
   };
 </script>
 
@@ -38,17 +70,17 @@
       </tr>
       <tr>
         <td>Status</td>
-        <td />
+        <td>{status}</td>
       </tr>
       <tr>
         <td>Size</td>
-        <td />
+        <td>{size}</td>
       </tr>
     </table>
 
     <div>
       <label for="sf-set">Number of correct answers</label>
-      <select id="sf-set">
+      <select id="sf-set" on:change={onSelect}>
         <option value="">-- Select --</option>
         <option value="0">Set 0</option>
         <option value="1">Set 1</option>
@@ -61,6 +93,6 @@
   <div class="buttons">
     <button class="button" on:click={onBack}>Back</button>
     <button class="button" on:click={() => onListing(topic)}>Listing</button>
-    <button class="button">Start</button>
+    <button class="button" disabled={startDisabled}>Start</button>
   </div>
 </div>
