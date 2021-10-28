4966caf5e2ae4eb12fcf2890e5aa48713e971e75
dead-end <dead-end@inbox.ru>
2021-10-28 23:15:57 +0200

    initial error handling

--- src/App.svelte
+++ src/App.svelte
@@ -2,19 +2,25 @@
   import { onMount } from 'svelte';
 
   import { viewStore } from './stores/viewStore';
+  import { errorStore } from './stores/errorStore';
 
   import { initApp } from './js/persist';
   import { pwaSerivceWorkerRegister } from './js/pwa';
 
   import Header from './components/Header.svelte';
   import Footer from './components/Footer.svelte';
+  import ErrorShow from './components/ErrorShow.svelte';
   import TopicList from './components/TopicList.svelte';
   import TopicShow from './components/TopicShow.svelte';
   import QuestList from './components/QuestList.svelte';
   import QuestShow from './components/QuestShow.svelte';
 
   onMount(() => {
-    pwaSerivceWorkerRegister();
+    try {
+      pwaSerivceWorkerRegister();
+    } catch (error) {
+      errorStore.addError(error);
+    }
 
     viewStore.views = {
       TopicList: {
@@ -42,12 +48,13 @@
 <div class="container">
   <Header />
 
+  {#if $errorStore.length !== 0}
+    <ErrorShow />
+  {/if}
+
   <div id="main" class="block">
     <svelte:component this={$viewStore.component} {...$viewStore.props} />
   </div>
 
   <Footer />
 </div>
-
-<style>
-</style>
---
+++ src/stores/errorStore.js
@@ -0,0 +1,39 @@
+import { writable } from 'svelte/store';
+
+/**
+ * The function creates a store object for errors. The errors are maintained in
+ * an array.
+ * 
+ * @returns The store object.
+ */
+const createErrorStore = () => {
+  //
+  // Initialize the store with an empty array.
+  //
+  const { subscribe, set, update } = writable([]);
+
+  return {
+    subscribe,
+
+    /**
+     * Add a new error to the error array.
+     * 
+     * @param {string} error 
+     */
+    addError: (error) => {
+      update(errors => {
+        errors.push(error)
+        return errors;
+      });
+    },
+
+    /**
+     * Reset the store with an empty array.
+     */
+    resetErrors: () => {
+      set([]);
+    },
+  };
+};
+
+export const errorStore = createErrorStore();
