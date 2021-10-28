b7e4c1c13109befc989f46dce8da8d2043beb64c
dead-end <dead-end@inbox.ru>
2021-10-28 23:16:30 +0200

    removed error handling

--- src/js/pwa.js
+++ src/js/pwa.js
@@ -5,19 +5,15 @@
 export const pwaSerivceWorkerRegister = async () => {
 
     if ('serviceWorker' in navigator) {
-        try {
-            //
-            // Finish registration, before we init app.
-            //
-            const registration = await navigator.serviceWorker.register(
-                'service-worker.js'
-            );
-            console.log(
-                'Registration of serivce worker done - scope:',
-                registration.scope
-            );
-        } catch (error) {
-            console.log('Service worker registration failed, error:', error);
-        }
+        //
+        // Finish registration, before we init app.
+        //
+        const registration = await navigator.serviceWorker.register(
+            'service-worker.js'
+        );
+        console.log(
+            'Registration of serivce worker done - scope:',
+            registration.scope
+        );
     }
 }
