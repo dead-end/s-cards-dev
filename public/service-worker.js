7adcab620eac96f1ddc866740d90d6d452b1c3a9
dead-end <dead-end@inbox.ru>
2021-10-29 18:09:18 +0200

    added simple versioned cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,15 +1,64 @@
 /**
- * TODO: initial version of the service worker, which does nothing.
+ * Cache name with a version. Each time the caches resources were changed, the 
+ * version has to be increased to ensure that the cache is invalidated.
  */
+const staticCacheName = 'site-static-v1';
 
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install', e);
+/**
+ * An array of assets the should be cached.
+ */
+const assets = [
+    // TODO: Removed from cache to make dev easier.
+    /*
+    '/',
+    '/index.html',
+    '/favicon.ico',
+    '/global.css',
+    '/build/bundle.css',
+    '/build/bundle.js',
+    */
+    'https://unpkg.com/sanitize.css@12.0.1/sanitize.css',
+    'https://unpkg.com/sanitize.css/forms.css'
+];
+
+/**
+ * Add an event listener for the install event.
+ */
+self.addEventListener('install', event => {
+    console.log('[Service Worker] Install', event);
+
+    event.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            cache.addAll(assets);
+        })
+    );
 });
 
-self.addEventListener('activate', (e) => {
-    console.log('[Service Worker] Activate', e);
+/**
+ * Add an event listener for the activate event.
+ */
+self.addEventListener('activate', event => {
+    console.log('[Service Worker] Activate', event);
+
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName)
+                .map(key => caches.delete(key))
+            );
+        })
+    );
 });
 
-self.addEventListener('fetch', (e) => {
-    console.log(`[Service Worker] Fetched resource ${e.request.url}`);
+/**
+ * Add an event listener for the activate event.
+ */
+self.addEventListener('fetch', event => {
+    console.log(`[Service Worker] Fetched resource ${event.request.url}`);
+
+    event.respondWith(
+        caches.match(event.request).then(cacheRes => {
+            return cacheRes || fetch(event.request);
+        })
+    );
 });
