7c24a1e2452c6d37fc89cc512663362b4d7c8fc5
dead end <dead.end@inbox.ru>
2021-11-07 23:13:56 +0100

    update

---
+++ src/stores/viewStore.ts
@@ -0,0 +1,38 @@
+import { writable } from 'svelte/store';
+
+/**
+ * The interface defines a view component.
+ */
+export interface View {
+  //
+  // The component is a class defined by svelte.
+  //
+  component: any;
+  //
+  // The properties used by the view, which are optional.
+  //
+  props?: Object;
+}
+
+/**
+ * The function creates the view store.
+ */
+const createViewStore = () => {
+  //
+  // Initialize the store with an empty object.
+  //
+  const { subscribe, set, update } = writable<View>({ component: '' });
+
+  return {
+    views: {},
+    subscribe,
+
+    setView: (id: string, props?: Object) => {
+      const view = viewStore.views[id];
+      view.props = props;
+      set(view);
+    },
+  };
+};
+
+export const viewStore = createViewStore();
