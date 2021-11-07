c0c83ea3500a88dd281c140475f7792e1ba01cf8
dead end <dead.end@inbox.ru>
2021-11-07 18:58:29 +0100

    update db config

--- src/js/dbConfig.ts
+++ src/js/dbConfig.ts
@@ -3,39 +3,38 @@ import { db } from './db';
 /**
  * The function gets the last modified date of the topics file from the config
  * store.
- *
- * @param {IDBDatabase} db The Database.
- * @returns A Promise for the last modified date.
  */
 export const dbcGetLastModified = () => {
 
-  return new Promise((resolve, reject) => {
-    db
+  return new Promise<void | Date>((resolve, reject) => {
+    const request = db
       .transaction(['config'], 'readonly')
       .objectStore('config')
-      .get('topics-last-modified').onsuccess = (e) => {
+      .get('topics-last-modified');
 
-        const prop = e.target.result;
+    request.onsuccess = (e) => {
+      //
+      // Get the value from the store
+      //
+      const prop = request.result;
 
-        //
-        // It is possible that no date exists in the store.
-        //
-        if (prop) {
-          resolve(prop.value);
-        } else {
-          resolve();
-        }
-      };
+      //
+      // It is possible that no date exists in the store.
+      //
+      if (prop) {
+        resolve(prop.value as Date);
+      } else {
+        resolve();
+      }
+    };
   });
 };
 
 /**
  * The function stores the last modified date for the topics file in the config
  * store.
- *
- * @param {Date} lastModified The last modified date of the topics file.
  */
-export const dbcSetLastModified = (lastModified) => {
+export const dbcSetLastModified = (lastModified: Date) => {
 
   const data = { key: 'topics-last-modified', value: lastModified };
   const store = db.transaction(['config'], 'readwrite').objectStore('config');
