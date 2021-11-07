d59c68e5f09ce2a3720390e6f381d2e43365acff
dead end <dead.end@inbox.ru>
2021-11-07 18:46:28 +0100

    update store

--- src/js/store.ts
+++ src/js/store.ts
@@ -2,25 +2,24 @@
  * The function is called with a transaction for a store, that has an index
  * with a given name. It deletes all elements from the store that have an index
  * with the value.
- *
- * @param {IDBTransaction} tx Transaction
- * @param {string} storeName Name of the store.
- * @param {string} idxName Name of the index.
- * @param {string} idxValue Value of the index.
- * @returns A Promise.
  */
-export const storeDeleteIndex = (tx, storeName, idxName, idxValue) => {
-  return new Promise((resolve, reject) => {
+export const storeDeleteIndex = (tx: IDBTransaction, storeName: string, idxName: string, idxValue: string) => {
+  return new Promise<void>((resolve, reject) => {
     //
     // Get the store from the transaction.
     //
     const store = tx.objectStore(storeName);
 
     //
+    // Get all keys for items that have the given value for the index.
+    //
+    const request = store.index(idxName).getAllKeys(idxValue);
+
+    //
     // Get an array with the keys of the matching objects,
     //
-    store.index(idxName).getAllKeys(idxValue).onsuccess = (e) => {
-      const keys = e.target.result;
+    request.onsuccess = (e) => {
+      const keys = request.result;
 
       //
       // Delete all objects with their keys,
@@ -42,14 +41,9 @@ export const storeDeleteIndex = (tx, storeName, idxName, idxValue) => {
 /**
  * The function is called with a transaction for a store with a given name and
  * an array of objects. All elements of the array are added to the store.
- *
- * @param {IDBTransaction} tx Transaction
- * @param {string} storeName Name of the store.
- * @param {array} arr Array with the objects.
- * @returns The function returns a Promise.
  */
-export const storeAddAll = (tx, storeName, arr) => {
-  return new Promise((resolve, reject) => {
+export const storeAddAll = (tx: IDBTransaction, storeName: string, arr: Array<Object>) => {
+  return new Promise<void>((resolve, reject) => {
     //
     // Get the store from the transaction.
     //
