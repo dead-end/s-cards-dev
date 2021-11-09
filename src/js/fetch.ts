461c8c4e86e89fc5762da72e49298c6c7957c263
dead end <dead.end@inbox.ru>
2021-11-09 10:11:14 +0100

    update

--- src/js/fetch.ts
+++ src/js/fetch.ts
@@ -3,24 +3,18 @@
 /**
  * The function fetches an url with json data and returns a Promise for that
  * json data.
- *
- * @param {String} url The url to fetch.
- * @returns A promise for the json data.
  */
-export const fetchJson = (url) => {
+export const fetchJson = (url: string) => {
   return fetch(url)
     .then((response) => response.json())
     .catch((e) => console.error(e));
 };
 
 /**
- * The funciton call an url with a HEAD request an returns the last modified
+ * The function calls an url with a HEAD request and returns the last modified
  * value as a Date instance.
- *
- * @param {String} url The url to fetch.
- * @returns A promise for the last modified Date
  */
-export const fetchLastModified = (url) => {
+export const fetchLastModified = (url: string) => {
   return fetch(url, { method: 'HEAD' })
     .then((response) => {
       //
