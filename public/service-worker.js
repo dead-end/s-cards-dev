b24f62b0b6fd722db14f6da91c368619b7f82f19
dead end <dead.end@inbox.ru>
2021-12-11 10:29:34 +0100

    update

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 /**
- * Cache name with a version. Each time the caches resources were changed, the 
+ * Cache name with a version. Each time the caches resources were changed, the
  * version has to be increased to ensure that the cache is invalidated.
  */
 const staticCacheName = 'site-static-v1';
@@ -7,14 +7,14 @@ const staticCacheName = 'site-static-v1';
 /**
  * The prefix differs for development: '' and production: '/s-cards'
  */
-const prefix = '/s-cards'
+const prefix = '/s-cards';
 
 /**
  * An array of assets the should be cached.
  */
 const assets = [
-    // TODO: Removed from cache to make dev easier.
-    /*
+  // TODO: Removed from cache to make dev easier.
+  /*
     prefix + '/',
     prefix + '/index.html',
     prefix + '/favicon.ico',
@@ -22,48 +22,58 @@ const assets = [
     prefix + '/build/bundle.css',
     prefix + '/build/bundle.js',
     */
-    'https://unpkg.com/sanitize.css@12.0.1/sanitize.css',
-    'https://unpkg.com/sanitize.css/forms.css'
+  'https://unpkg.com/sanitize.css@12.0.1/sanitize.css',
+  'https://unpkg.com/sanitize.css/forms.css',
 ];
 
 /**
  * Add an event listener for the install event.
  */
-self.addEventListener('install', event => {
-    console.log('[Service Worker] Install', event);
+self.addEventListener('install', (event) => {
+  console.log('[Service Worker] Install', event);
 
-    event.waitUntil(
-        caches.open(staticCacheName).then(cache => {
-            cache.addAll(assets);
-        })
-    );
+  //
+  // Fill the cache identified with our label with the assets.
+  //
+  event.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      cache.addAll(assets);
+    })
+  );
 });
 
 /**
  * Add an event listener for the activate event.
  */
-self.addEventListener('activate', event => {
-    console.log('[Service Worker] Activate', event);
+self.addEventListener('activate', (event) => {
+  console.log('[Service Worker] Activate', event);
 
-    event.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(keys
-                .filter(key => key !== staticCacheName)
-                .map(key => caches.delete(key))
-            );
-        })
-    );
+  //
+  // Remove everything from the cache, which has not our label.
+  //
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== staticCacheName)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
 });
 
 /**
  * Add an event listener for the activate event.
  */
-self.addEventListener('fetch', event => {
-    console.log(`[Service Worker] Fetched resource ${event.request.url}`);
+self.addEventListener('fetch', (event) => {
+  console.log(`[Service Worker] Fetched resource ${event.request.url}`);
 
-    event.respondWith(
-        caches.match(event.request).then(cacheRes => {
-            return cacheRes || fetch(event.request);
-        })
-    );
+  //
+  // If we find something in the cache, then we use it.
+  //
+  event.respondWith(
+    caches.match(event.request).then((cacheRes) => {
+      return cacheRes || fetch(event.request);
+    })
+  );
 });
