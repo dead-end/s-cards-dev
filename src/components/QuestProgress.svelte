4749228e577e5440da4aced89c8f01c9478c3ef2
dead end <dead.end@inbox.ru>
2021-12-25 23:25:06 +0100

    update

--- src/components/QuestProgress.svelte
+++ src/components/QuestProgress.svelte
@@ -5,12 +5,11 @@
 </script>
 
 <span class="h6">
-  Progress: <span class="is-text-success">{quest.progress}</span>
+  Progress: <span class="is-text-success">{quest.progress}</span> /
+  <span class="is-text-success">3</span>
 
   <span class="hide-sm">
-    (Total: <span class="is-text-success">{quest.total}</span> /
-    <span class="is-text-danger">{quest.failed}</span>
-    / Wrong:
-    <span>{quest.ratio}%</span>)
+    Total: <span class="is-text-success">{quest.total}</span> Wrong:
+    <span class="is-text-danger">{quest.ratio}%</span>
   </span>
 </span>
