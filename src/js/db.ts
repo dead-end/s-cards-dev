e36f5aecc8e8069a2d6ef9695e604d2a8beb454b
dead end <dead.end@inbox.ru>
2022-01-07 18:09:24 +0100

    add index

--- src/js/db.ts
+++ src/js/db.ts
@@ -32,6 +32,7 @@ const nullToOne = (event: IDBVersionChangeEvent) => {
     keyPath: 'id', autoIncrement: true
   });
   storeQuest.createIndex('file', 'file', { unique: false });
+  storeQuest.createIndex('ratio', 'ratio', { unique: false });
 
   //
   // Create config store
