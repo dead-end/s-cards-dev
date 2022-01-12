adb8eef2cb57cfb94b28190ea5a536f6b4080493
dead end <dead.end@inbox.ru>
2022-01-12 21:41:37 +0100

    added fetchhash function

--- src/js/fetch.ts
+++ src/js/fetch.ts
@@ -17,13 +17,16 @@ export const fetchJson = (url: string) => {
       }
 
       return response.json();
-    }).catch((e) => errorStore.addError(e));
+    }).catch((e) => errorStore.addError('fetchJson: ' + e));
 };
 
 /**
  * The function calls an url with a HEAD request and returns the last modified
  * value as a Date instance.
+ * 
+ * The lastmodified value seam to change on every commit to the repository.
  */
+// TODO: maybe use fetchHash and store lastloaded
 export const fetchLastModified = (url: string) => {
   return fetch(url, { method: 'HEAD' })
     .then((response) => {
@@ -47,5 +50,34 @@ export const fetchLastModified = (url: string) => {
       if (lastModified) {
         return new Date(lastModified);
       }
-    }).catch((e) => errorStore.addError(e));
+    }).catch((e) => errorStore.addError('fetchLastModified: ' + e));
+};
+
+/**
+ * The function sends a HEAD request to the server and returns a hash value, in
+ * this case simply the size of the file. If the hash has changed, the file has
+ * to be reloaded.
+ * 
+ * The lastmodified or etag cannot be used. They seam to change on every commit
+ * to the repository.
+ */
+export const fetchHash = (url: string) => {
+  return fetch(url, { method: 'HEAD' })
+    .then((response) => {
+
+      //
+      // Ensure that the request is ok.
+      //
+      if (!response.ok) {
+        throw Error(`Unable to get hash for: ${url} - ${response.statusText}`);
+      }
+
+      //
+      // Get the hash value from the response.
+      //
+      const hash = response.headers.get('Content-Length');
+      console.log('url: ', url, 'header:', hash);
+
+      return hash;
+    }).catch((e) => errorStore.addError('fetchHash: ' + e));
 };
