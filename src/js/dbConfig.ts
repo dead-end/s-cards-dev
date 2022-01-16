a47403c09b19cbf3595c855f42d98428f94ce21a
dead end <dead.end@inbox.ru>
2022-01-16 10:50:04 +0100

    using generics

--- src/js/dbConfig.ts
+++ src/js/dbConfig.ts
@@ -1,32 +1,33 @@
-// TODO: TS OK => remove TODO
 import { db } from './db';
 
 /**
  * The config collection contains key value pairs.
  */
-export interface Config {
+export interface Config<T> {
   key: string,
-  value: any
+  value: T
 }
 
 /**
- * The function reads a Config from the store.
+ * The function reads a Config from the store, which may not exist.
  */
-export const dbcGetConfig = (key: string) => {
+export const dbcGetConfig = <T>(key: string) => {
 
-  return new Promise<void | Config>((resolve, reject) => {
+  return new Promise<void | Config<T>>((resolve, reject) => {
 
-    const request = db
+    const store = db
       .transaction(['config'], 'readonly')
-      .objectStore('config')
-      .get(key);
+      .objectStore('config');
 
+    const request = store.get(key);
     request.onsuccess = (e) => {
       //
       // Get the value from the store. It is possible that no date exists in
       // the store.
       //
-      resolve(request.result as Config);
+      const config: Config<T> = request.result
+      console.log('Store:', store.name, 'get config key:', config.key, 'value:', config.value);
+      resolve(config);
     };
   });
 };
@@ -34,11 +35,11 @@ export const dbcGetConfig = (key: string) => {
 /**
  * The function persists a Config in the store.
  */
-export const dbcSetConfig = (config: Config) => {
+export const dbcSetConfig = <T>(config: Config<T>) => {
 
   const store = db.transaction(['config'], 'readwrite').objectStore('config');
 
   store.put(config).onsuccess = () => {
-    console.log('Store:', store.name, 'config key:', config.key, 'value:', config.value);
+    console.log('Store:', store.name, 'set config key:', config.key, 'value:', config.value);
   };
 };
