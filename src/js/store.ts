62c1c3982fedf5097e1d15232bbd3dd4df37bb03
dead end <dead.end@inbox.ru>
2022-02-04 10:29:55 +0100

    update

--- src/js/store.ts
+++ src/js/store.ts
@@ -1,64 +1,84 @@
-// TODO: TS OK => remove TODO
 /**
  * The function is called with a transaction for a store, that has an index
  * with a given name. It deletes all elements from the store that have an index
  * with the value.
  */
 export const storeDeleteIndex = (tx: IDBTransaction, storeName: string, idxName: string, idxValue: string) => {
+
   return new Promise<void>((resolve, reject) => {
+
     //
     // Get the store from the transaction.
     //
-    const store = tx.objectStore(storeName);
+    const store = tx.objectStore(storeName)
 
     //
     // Get all keys for items that have the given value for the index.
     //
-    const request = store.index(idxName).getAllKeys(idxValue);
+    const request = store.index(idxName).getAllKeys(idxValue)
 
     //
     // Get an array with the keys of the matching objects,
     //
     request.onsuccess = (e) => {
-      const keys = request.result;
+      const keys = request.result
 
       //
       // Delete all objects with their keys,
       //
       keys.forEach((key) => {
-        store.delete(key).onsuccess = (e) => {
-          console.log('Store:', store.name, 'deleted:', key);
-        };
-      });
+        storeDel(store, key)
+      })
 
       //
       // Resolve after all elements are deleted.
       //
-      resolve();
-    };
-  });
-};
+      resolve()
+    }
+  })
+}
 
 /**
- * The function is called with a transaction for a store with a given name and
- * an array of objects. All elements of the array are added to the store.
+ * Simple wrapper that deletes an object by its id.
  */
-export const storeAddAll = (tx: IDBTransaction, storeName: string, arr: Array<Object>) => {
-  return new Promise<void>((resolve, reject) => {
-    //
-    // Get the store from the transaction.
-    //
-    const store = tx.objectStore(storeName);
+export const storeDel = (store: IDBObjectStore, id: IDBValidKey) => {
+  const request = store.delete(id)
 
-    arr.forEach((item) => {
-      store.add(item).onsuccess = (e) => {
-        console.log('Store:', store.name, 'added:', item);
-      };
-    });
+  request.onsuccess = (e) => {
+    console.log('Store:', store.name, 'delete:', id)
+  }
 
-    //
-    // Resolve after all elements are added.
-    //
-    resolve();
-  });
-};
+  request.onerror = (e) => {
+    console.log('Store:', store.name, 'delete:', id, 'error', e)
+  }
+}
+
+/**
+ * Simple wrapper that inserts an object.
+ */
+export const storeAdd = (store: IDBObjectStore, obj: any) => {
+  const request = store.add(obj)
+
+  request.onsuccess = (e) => {
+    console.log('Store:', store.name, 'add:', obj)
+  }
+
+  request.onerror = (e) => {
+    console.log('Store:', store.name, 'add:', obj, 'error', e)
+  }
+}
+
+/**
+ * Simple wrapper that updates an object.
+ */
+export const storePut = (store: IDBObjectStore, obj: any) => {
+  const request = store.put(obj)
+
+  request.onsuccess = (e) => {
+    console.log('Store:', store.name, 'put:', obj)
+  }
+
+  request.onerror = (e) => {
+    console.log('Store:', store.name, 'put:', obj, 'error', e)
+  }
+}
