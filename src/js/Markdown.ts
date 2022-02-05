ba6402f62eb6f28609a12ec154146b4cc17e1c0e
dead end <dead.end@inbox.ru>
2022-02-05 23:50:52 +0100

    update

--- src/js/Markdown.ts
+++ src/js/Markdown.ts
@@ -4,25 +4,25 @@
  * function ensures that the result is balanced.
  ******************************************************************************/
 class Mapping {
-  md: string;
-  tag: string;
-  count = 0;
+  md: string
+  tag: string
+  count = 0
 
   constructor(md: string, tag: string) {
-    this.md = md;
-    this.tag = tag;
+    this.md = md
+    this.tag = tag
   }
 
   getTag() {
-    return ++this.count % 2 ? `<${this.tag}>` : `</${this.tag}>`;
+    return ++this.count % 2 ? `<${this.tag}>` : `</${this.tag}>`
   }
 
   check() {
     if (this.count % 2) {
-      throw new Error(`Unbalanced tag: ${this.md} count: ${this.count}`);
+      throw new Error(`Unbalanced tag: ${this.md} count: ${this.count}`)
     }
 
-    this.count = 0;
+    this.count = 0
   }
 }
 
@@ -39,51 +39,51 @@ class Mapping {
  ******************************************************************************/
 
 export default class Markdown {
-  map = {};
-  regex: RegExp;
+  map = {}
+  regex: RegExp
 
   constructor() {
 
-    this._register('_', 'u');
-    this._register('*', 'b');
-    this._register('~', 'i');
+    this._register('_', 'u')
+    this._register('*', 'b')
+    this._register('~', 'i')
 
-    this.regex = this._pattern();
+    this.regex = this._pattern()
   }
 
   _register(chr: string, tag: string) {
-    this.map[chr] = new Mapping(chr, tag);
+    this.map[chr] = new Mapping(chr, tag)
   }
 
   _pattern() {
-    let result = '';
+    let result = ''
     for (let m in this.map) {
-      result += m;
+      result += m
     }
 
-    return new RegExp(`[${result}]`, 'g');
+    return new RegExp(`[${result}]`, 'g')
   }
 
   tag(chr: string) {
     if (!this.map.hasOwnProperty(chr)) {
-      throw new Error('Unknown element: ' + chr);
+      throw new Error('Unknown element: ' + chr)
     }
 
-    return this.map[chr].getTag();
+    return this.map[chr].getTag()
   }
 
   _check() {
     for (let m in this.map) {
-      this.map[m].check();
+      this.map[m].check()
     }
   }
 
   _substitute(line: string) {
-    const me = this;
+    const me = this
 
     return line.replaceAll(this.regex, function (elem) {
-      return me.tag(elem);
-    });
+      return me.tag(elem)
+    })
   }
 
   /****************************************************************************
@@ -93,46 +93,46 @@ export default class Markdown {
 
   toHtml(lines: string | string[]) {
     if (!Array.isArray(lines)) {
-      lines = [lines];
+      lines = [lines]
     }
 
-    let inside = false;
+    let inside = false
 
-    let result = '';
+    let result = ''
 
     for (let line of lines) {
       if (line.startsWith('- ')) {
         if (!inside) {
-          result += '<ul>';
-          inside = true;
+          result += '<ul>'
+          inside = true
         }
 
-        result += '<li>' + this._substitute(line.substring(2).trim()) + '</li>';
-        continue;
+        result += '<li>' + this._substitute(line.substring(2).trim()) + '</li>'
+        continue
       }
 
       if (inside) {
-        result += '</ul>';
-        inside = false;
+        result += '</ul>'
+        inside = false
       } else if (result !== '') {
-        result += '<br />';
+        result += '<br />'
       }
 
-      result += this._substitute(line.trim());
+      result += this._substitute(line.trim())
     }
 
     //
     // If the array ends with list, we need the closing tag.
     //
     if (inside) {
-      result += '</ul>';
+      result += '</ul>'
     }
 
     //
     // ensure that all is balanced
     //
-    this._check();
+    this._check()
 
-    return result;
+    return result
   }
 }
