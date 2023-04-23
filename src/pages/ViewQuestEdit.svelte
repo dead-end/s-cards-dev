30be266c74790aee6386dcf63eb5e9c0adc7f5d6
Dead End <dead.end@inbox.ru>
2023-04-23 11:01:28 +0200

    new file

---
+++ src/pages/ViewQuestEdit.svelte
@@ -0,0 +1,95 @@
+<script lang="ts">
+  import type { Question, QuestionJson } from '../js/questModel';
+  import Markdown from '../js/Markdown';
+  import { arrayToString, stringToArray } from '../js/utils';
+  import { viewStore } from '../stores/viewStore';
+  import { repoGetJson, repoWriteJson } from '../js/repo';
+  import { errorStore } from '../stores/errorStore';
+
+  const md = new Markdown();
+
+  export let question: Question;
+
+  let questHtml = '';
+  let answerHtml = '';
+
+  let questStr = arrayToString(question.quest);
+  let answerStr = arrayToString(question.answer);
+
+  $: questHtml = md.toHtml(stringToArray(questStr));
+  $: answerHtml = md.toHtml(stringToArray(answerStr));
+
+  let comment = `Update question: '${question.id}' in: '${question.file}'`;
+
+  /**
+   * Do the update
+   */
+  const handleSubmit = async () => {
+    const readResult = await repoGetJson<QuestionJson[]>(question.file);
+    if (readResult.hasError()) {
+      errorStore.addError(readResult.getMessage());
+      return;
+    }
+
+    const value = readResult.getValue();
+
+    for (let i = 0; i < value.json.length; i++) {
+      const qa = value.json[i];
+      if (qa.id === question.id) {
+        qa.quest = stringToArray(questStr);
+        qa.answer = stringToArray(answerStr);
+        break;
+      }
+    }
+
+    const writeResult = await repoWriteJson(
+      question.file,
+      value.json,
+      value.hash,
+      comment
+    );
+    if (writeResult.hasError()) {
+      errorStore.addError(writeResult.getMessage());
+      return;
+    }
+
+    viewStore.setView('ViewTopicList', { id: '' });
+  };
+
+  /**
+   * Back to topic list
+   */
+  const onBack = () => {
+    viewStore.setView('ViewTopicList', { id: '' });
+  };
+</script>
+
+<div class="card card-shadow content">
+  <form on:submit|preventDefault={handleSubmit}>
+    <div class="block">
+      <div class="h5">Question: {question.id}</div>
+      <div class="grid grid-2">
+        <textarea rows="10" id="quest" bind:value={questStr} class="input" />
+        <div class="card content is-primary">
+          <p>{@html questHtml}</p>
+        </div>
+      </div>
+
+      <h5>Answer</h5>
+      <div class="grid grid-2">
+        <textarea rows="10" id="answer" bind:value={answerStr} class="input" />
+        <div class="card content is-primary">
+          <p>{@html answerHtml}</p>
+        </div>
+      </div>
+
+      <h5>Comment</h5>
+      <input type="input" bind:value={comment} class="input" />
+    </div>
+
+    <div class="is-floating">
+      <button class="button" on:click={onBack}>Cancel</button>
+      <button class="button">Save</button>
+    </div>
+  </form>
+</div>
